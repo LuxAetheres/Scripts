a73447563a7395f2c61c6ac213cf2fd8d68d480e
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-04-25 03:28:15 +0200

    Add files via upload

---
+++ LuxScript_Yens.lua
@@ -0,0 +1,41 @@
+local function suppresslag()
+    game:GetService("Workspace").MouseIgnore:remove()
+    print("Chest Entity Removed")
+end
+local function notif(text)
+    game.StarterGui:SetCore("SendNotification", {Title = "LuxScript";Text = text;Icon = "rbxassetid://3354469513";Duration = 5;})
+end
+local function lspin(n)
+    for i = 1,n do
+        task.spawn(function()
+            game:GetService("ReplicatedStorage").RSPackage.Events.GeneralFunction:InvokeServer("LotteryRandomDraw")
+            game:GetService("ReplicatedStorage").RSPackage.Events.GeneralEvent:FireServer("SpinFinished")
+        end)
+    end
+end
+local function sspin()
+    for i = 1,3 do
+        task.spawn(function()
+            lspin(1000)
+        end)
+    end
+end
+local function totalspin()
+    for i = 1,3 do
+        for i = 1,2 do
+            task.spawn(function()
+                sspin()
+            end)
+            wait(10)
+        end
+    end
+end
+if _G.LuxScript_Chest == true then suppresslag() notif("Chest deleted.") end
+_G.Chest = 0
+while _G.LuxScript_Yens == true do totalspin() _G.Chest = _G.Chest + 20000 notif("Opened : ~"..tostring(_G.Chest)) end
+
+
+
+
+
+
