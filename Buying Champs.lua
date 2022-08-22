7b91459d4ae03c7dc5b48c3058325aa154507fd1
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-23 01:37:19 +0200

    Update Buying Champs.lua

--- Buying Champs.lua
+++ Buying Champs.lua
@@ -1,4 +1,4 @@
-fireclickdetector(game:GetService("Workspace").Shops["SaiyanPod2"].ClickBox.ClickDetector)
+fireclickdetector(game:GetService("Workspace").Shops[pod].ClickBox.ClickDetector)
 wait()
 for k,v in next,getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.Category.Frames.SpecialObtain.Container.Buy.MouseButton1Click) do
 	v.Function()
