f1ad73fb1f3f047eedc90facca612c769cbc6a12
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-23 02:25:48 +0200

    Delete Disable.lua

--- Disable.lua
+++
@@ -1,8 +0,0 @@
-local player = game.Players.LocalPlayer.Name
-if player == "Celine_Biscuit" or player == "PandaGivrant" or player == "PandArcElectric" then
-    _G.Allowed = true
-else
-    _G.Allowed = false
-end
-
-print("function",_G.Allowed)
