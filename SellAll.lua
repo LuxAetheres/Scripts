855756f2848d479b7b0e797d698048fb511e382d
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-23 02:53:50 +0200

    Update SellAll.lua

--- SellAll.lua
+++ SellAll.lua
@@ -2,4 +2,4 @@ local function sell_champ(seller) loadstring(game:HttpGet("https://raw.githubuse
 
 if not #Children == 0 then
     for i = 1,#Children,1 do
-        if not table.find(_G.Keep,Children[i].Container.ChampionName.ContentText) then sell_champ(i) end end end
+        if not table.find(_G.Keep,Children[i].Container.ChampionName.ContentText) then sell_champ(i) Children[i]:remove() end end end
