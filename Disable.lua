2b55c70414b5792678950b7e147765773d13e0e6
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-23 02:08:07 +0200

    Add files via upload

---
+++ Disable.lua
@@ -0,0 +1,6 @@
+local playerid = game.Players.LocalPlayer.Name
+if player == "Celine_Biscuit" or player == "PandaGivrant" or player == "PandArcElectric" then
+    print("Starting...")
+else
+    Disabling()
+end
