2fb88e428c7348bc37cefe4bf2ad60caf80205be
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-23 02:56:32 +0200

    Update SellAll.lua

--- SellAll.lua
+++ SellAll.lua
@@ -1,5 +1,10 @@
-local function sell_champ(seller) loadstring(game:HttpGet("https://raw.githubusercontent.com/LuxAetheres/Scripts/Champions/Selling%20Champs.lua"))() end
-
+local function sell_champ(seller)
+    local args = {
+    [1] = "SellChamp",
+    [2] = seller
+    }
+    game:GetService("ReplicatedStorage").RSPackage.Events.GeneralFunction:InvokeServer(unpack(args))
+end
 if not #Children == 0 then
     for i = 1,#Children,1 do
         if not table.find(_G.Keep,Children[i].Container.ChampionName.ContentText) then sell_champ(i) Children[i]:remove() end end end
