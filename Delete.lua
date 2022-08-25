c1103f7c255f689e4df7e14bd0be04f57494dfde
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-25 02:34:16 +0200

    Delete Delete.lua

--- Delete.lua
+++
@@ -1,5 +0,0 @@
-local champlist = game.Players.localPlayer.PlayerGui.Main.Category.Frames.Champions.Container.List
-local Children = champlist:GetChildren()
-if not #Children == 0 then
-    for i = 1,#Children,1 do
-        if Children[i] == "UIGrindLayout" then Children[i]:remove() end end end
