31bd86d0fcadd26f111dfefd4adbade52917a380
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-25 02:25:24 +0200

    Update Local.lua

--- Local.lua
+++ Local.lua
@@ -2,7 +2,6 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("Auto Grind Champs", "Synapse")
 local MainTab = Window:NewTab("Champions")
 local AutoOpen = MainTab:NewSection("Activate Auto-Open-Champions")
-local function buy_champ(pod) loadstring(game:HttpGet("https://raw.githubusercontent.com/LuxAetheres/Scripts/Champions/Buying%20Champs.lua"))() end
-local function sell_champ() loadstring(game:HttpGet("https://raw.githubusercontent.com/LuxAetheres/Scripts/Champions/Selling%20Champs.lua"))() end
+local function imported_function(funct)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuxAetheres/Scripts/Champions/"..funct..".lua"))() end
 local Opening = false
-local Selling = false
