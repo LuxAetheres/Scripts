142bcfbf4a474dd6ad4c8b1f6f3efae62e1e7051
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-25 21:09:42 +0200

    Delete Local.lua

--- Local.lua
+++
@@ -1,7 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Auto Grind Champs", "Synapse")
-local MainTab = Window:NewTab("Champions")
-local AutoOpen = MainTab:NewSection("Activate Auto-Open-Champions")
-local function imported_function(funct)
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuxAetheres/Scripts/Champions/"..funct..".lua"))() end
-local Opening = false
