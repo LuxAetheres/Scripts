2af1354fdabf4fd7e3c8fd5cb8f8a0f95255aa9f
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-25 21:09:34 +0200

    Delete Buy.lua

--- Buy.lua
+++
@@ -1,5 +0,0 @@
-fireclickdetector(game:GetService("Workspace").Shops.SaiyanPod2.ClickBox.ClickDetector)
-wait()
-for k,v in next,getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.Category.Frames.SpecialObtain.Container.Buy.MouseButton1Click) do
-	v.Function()
-end
