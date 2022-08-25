75e9b124ab1e849c271223581ecd8df4a812cecf
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-25 19:25:29 +0200

    Update Sell.lua

--- Sell.lua
+++ Sell.lua
@@ -1,5 +1,6 @@
 local args = {
     [1] = "SellChamp",
     [2] = Children[#Children]
+    print(Children[#Children])
 }
 game:GetService("ReplicatedStorage").RSPackage.Events.GeneralFunction:InvokeServer(unpack(args))
