32fc157b0783910050b384b6486db317f0419526
LuxAetheres <100476805+LuxAetheres@users.noreply.github.com>
2022-08-25 21:10:15 +0200

    Delete Buying Champs.lua

--- Buying Champs.lua
+++
@@ -1,5 +0,0 @@
-fireclickdetector(game:GetService("Workspace").Shops[pod].ClickBox.ClickDetector)
-wait()
-for k,v in next,getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.Category.Frames.SpecialObtain.Container.Buy.MouseButton1Click) do
-	v.Function()
-end
